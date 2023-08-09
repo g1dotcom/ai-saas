ee5f34dc28b0b683b56769db91b80e1adba9f0aa
Gökhan Süle <thegkhnsl@gmail.com>
2023-08-09 14:11:07 +0300

    🎨 style(layout.tsx): import and use Sidebar component in DashboardLayout to improve code organization and readability
    🚀 feat(sidebar.tsx): create Sidebar component to display a sidebar in the DashboardLayout
    
    🎨 style(navbar.tsx): import and use UserButton component from @clerk/nextjs to display a user button in the Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,7 @@
 import { Menu } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -8,6 +9,9 @@ const Navbar = () => {
       <Button variant="ghost" size="icon" className="md:hidden">
         <Menu />
       </Button>
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
   );
 };
