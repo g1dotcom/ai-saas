5209423002437fbc6f19f65251aa3ec20c321b14
Gökhan Süle <thegkhnsl@gmail.com>
2023-08-09 14:38:06 +0300

    🎨 style(sidebar.tsx): update Sidebar component layout and styling 🆕 feat(sidebar.tsx): add logo to Sidebar component with a link to the dashboard page 🆕 feat(logo.png): add logo image file for the Sidebar component

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,9 +1,29 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const montserrat = Montserrat({
+  weight: "600",
+  subsets: ["latin"],
+});
+
 const Sidebar = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-      Hello Sidebar Component
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative w-8 h-8 mr-4">
+            <Image fill alt="logo" src="/logo.png" />
+          </div>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            {" "}
+            Genıus
+          </h1>
+        </Link>
+      </div>
     </div>
   );
 };
