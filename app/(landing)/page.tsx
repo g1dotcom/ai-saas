89a80a7b4c306e1b7396fe94add7fb48f5273e5a
Gökhan Süle <thegkhnsl@gmail.com>
2023-09-28 15:46:17 +0300

    🐛 fix(page.tsx): fix typo in LandingPage component name from "LandingPagee" to "LandingPage" for consistency and correctness

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const LandingPage = () => {
   return (
     <div>
-      LandingPage
+      LandingPagee
       <div>
         <Link href="/sign-in">
           <Button>Login</Button>
