7f4ffe3fc84941dc9cdfcf019b88aa7da99f2956
Gökhan Süle <thegkhnsl@gmail.com>
2023-09-28 16:20:54 +0300

    📝 chore(route.ts): fix typo in instruction message to improve clarity and readability

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -10,7 +10,7 @@ const openai = new OpenAIApi(configuration);
 
 const instructionMessage: ChatCompletionRequestMessage = {
     role:"system",
-    content:"You are a cde generator. You must answer only in markdown code snippets. Use code comments for explanations."
+    content:"You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations."
 }
 
 export async function POST(
