7129a3421bfab8e99eb3b458491f839ecd34a520
Gökhan Süle <thegkhnsl@gmail.com>
2023-10-02 12:00:50 +0300

    🔧 fix(image/page.tsx): import missing Download icon from lucide-react to fix rendering issue 🔧 fix(image/page.tsx): add missing imports for Card, CardFooter, and Image components to fix rendering issue 🔧 fix(image/page.tsx): update image rendering section to display images in a grid layout with download button ✨ feat(api/image/route.ts): add new API route for creating images using OpenAI API

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import axios from "axios";
 import * as z from "zod";
-import { ImageIcon, MessageSquare } from "lucide-react";
+import { Download, ImageIcon, MessageSquare } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -25,6 +25,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Card, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
 
 const ImagePage = () => {
   const router = useRouter();
@@ -172,7 +174,25 @@ const ImagePage = () => {
           {images.length === 0 && !isLoading && (
             <Empty label="No images generated." />
           )}
-          <div className="">Images will be rendered here</div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+            {images.map((src) => (
+              <Card key={src} className="rounded-lg overflow-hidden">
+                <div className="relative aspect-square">
+                  <Image alt="Image" fill src={src} />
+                </div>
+                <CardFooter className="p-2">
+                  <Button
+                    onClick={() => window.open(src)}
+                    variant="secondary"
+                    className="w-full"
+                  >
+                    <Download className="h-4 w-4 mr-2" />
+                    Download
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
